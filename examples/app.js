002532f71af94cfd388ebcdb0a9d9166f6da6730
Bart Teeuwisse <bart.teeuwisse@thecodemill.biz>
2010-11-16 11:38:59 -0800

    - Renamed express-session-mongo to connect-session-mongo.

--- examples/app.js
+++ examples/app.js
@@ -3,14 +3,14 @@
 //Needed for monit/upstart
 process.chdir(__dirname);
 
-var xp = require('express'),
-    MongoStore = require('express-session-mongo');
+var connect = require('connect'),
+    MongoStore = require('connect-session-mongo');
 
-var app = module.exports = xp.createServer();
+var app = module.exports = connect.createServer();
 
 app.configure(function(){
-    app.use(xp.cookieDecoder());
-    app.use(xp.session({ store: new MongoStore() }));
+    app.use(connect.cookieDecoder());
+    app.use(connect.session({ store: new MongoStore() }));
     app.use(app.router);
 });
 
